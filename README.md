24be9a7a5c72d91d5a75554fc03ff70701fa8585
Diego Najar <dignajar@gmail.com>
2017-08-22 00:42:34 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bludit Themes Homepage
+This repository contains the files of the website Bludit Themes.
+Feel free to create a pull request with improves, languages translations, etc.
+
+English:
+- https://themes.bludit.com
+
+Deutsch (German):
+- https://themes.bludit.com/de/
+
+Español (Spanish):
+- https://themes.bludit.com/es/
---
+++ config.php
@@ -0,0 +1,81 @@
+<?php
+define('BLUDIT', true);
+define('DS', DIRECTORY_SEPARATOR);
+define('PATH_ROOT', __DIR__.DS);
+define('CHARSET', 'UTF-8');
+define('DOMAIN', 'https://themes.bludit.com');
+define('FILES', PATH_ROOT.'files'.DS);
+
+// TOP Bar links
+$topbar = array(
+	'download'=>'https://www.bludit.com#download',
+	'demo'=>'https://www.bludit.com#demo',
+	'docs'=>'https://docs.bludit.com',
+	'themes'=>'https://themes.bludit.com',
+	'plugins'=>'https://plugins.bludit.com',
+	'donations'=>'https://www.bludit.com#donations',
+	'pro'=>'https://pro.bludit.com'
+);
+
+// Language
+$defaultLanguage = 'en';
+$acceptedLanguages = array('en', 'de', 'es');
+if (isset($_GET['l'])) {
+	if (in_array($_GET['l'], $acceptedLanguages)) {
+		$defaultLanguage = $_GET['l'];
+	}
+}
+
+$jsonData = file_get_contents(PATH_ROOT.'languages'.DS.$defaultLanguage.'.json');
+$languageArray = json_decode($jsonData, true);
+
+function l($key, $print=true) {
+	global $languageArray;
+	$key = mb_strtolower($key, CHARSET);
+	$key = str_replace(' ','-',$key);
+	if (isset($languageArray[$key])) {
+		if ($print) {
+			echo $languageArray[$key];
+		} else {
+			return $languageArray[$key];
+		}
+	}
+}
+
+// Locale
+$defaultLocale = 'en_US';
+if ($defaultLanguage == "es") {
+	$defaultLocale = 'es_ES';
+	$topbar = array(
+		'download'=>'https://www.bludit.com/es/#download',
+		'demo'=>'https://www.bludit.com/es/#demo',
+		'docs'=>'https://docs.bludit.com/es/',
+		'themes'=>'https://themes.bludit.com/es/',
+		'plugins'=>'https://plugins.bludit.com/es/',
+		'donations'=>'https://www.bludit.com/es/#donations',
+		'pro'=>'https://pro.bludit.com/es/'
+	);
+} elseif ($defaultLanguage == "de") {
+	$defaultLocale = 'de_DE';
+	$topbar = array(
+		'download'=>'https://www.bludit.com/de/#download',
+		'demo'=>'https://www.bludit.com/de/#demo',
+		'docs'=>'https://docs.bludit.com/de/',
+		'themes'=>'https://themes.bludit.com/de/',
+		'plugins'=>'https://plugins.bludit.com/de/',
+		'donations'=>'https://www.bludit.com/de/#donations',
+		'pro'=>'https://pro.bludit.com/de/'
+	);
+}
+
+//
+function listDirectories($path, $regex='*', $sortByDate=false) {
+        $directories = glob($path.$regex, GLOB_ONLYDIR);
+        if(empty($directories)) {
+                return array();
+        }
+        if($sortByDate) {
+                usort($directories, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+        }
+        return $directories;
+}
